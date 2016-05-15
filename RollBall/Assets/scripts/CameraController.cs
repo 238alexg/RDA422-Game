77fab38c8355231223aecf92271d21aaf52f396e
Dustin <dustinnr@gmail.com>
2016-05-15 15:18:30 -0700

    added rollball

---
+++ RollBall/Assets/scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame. runs after all items have been processed in update. 
+	// This way the player movement has been updated
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ RollBall/Assets/scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate(){
+
+		float moveH = Input.GetAxis ("Horizontal");
+		float moveV = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveH, 0.0f, moveV);
+		rb.AddForce (movement*speed);
+	}
+}
