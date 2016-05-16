4ff1bad6ab981806e0af56accb904b8c0365bd29
Alex <alexgeoffrey@128-223-223-89-wireless.uoregon.edu>
2016-05-15 17:27:38 -0700

    First Commit for Our Game
    Completed tutorial, added enemy object with health that gets destroyed
    after being hit with rocket object twice.

---
+++ assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public float health;
+
+	void Start () {
+		health = 2000.0f;
+
+	}
+
+	void Update () {
+		if (health <= 0.0) {
+			Destroy(gameObject);
+		}
+	}
+
+	// find out if it gets hit
+	void OnCollisionEnter(Collision c) {
+		Debug.Log (c.collider.name);
+		Debug.Log (c.collider.tag);
+
+		if (c.collider.tag == "Rocket") {
+			Debug.Log ("ROCKET HIT");
+
+			health = health - 1000.0f;
+		}
+	}
+}
