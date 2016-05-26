cbc36546a8ce9d1dd96e5cbcafdf0ef19fed39f9
Dustin <dustinnr@gmail.com>
2016-05-26 14:58:33 -0700

    .

--- assets/Enemy.cs
+++ assets/Enemy.cs
@@ -1,30 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
 	public float health;
+	public Text score_text;
+	public GameObject ActualScore;
+	public int score;
 
 	void Start () {
+		ActualScore = GameObject.FindWithTag ("Player");
+		score_text = ActualScore.GetComponent <Text>();
+		score = 0;
+		score_text.text = "" + score;
 		health = 2000.0f;
-
 	}
 
 	void Update () {
 		if (health <= 0.0) {
+			ActualScore = GameObject.FindWithTag ("HUD");
+			score_text = ActualScore.GetComponent <Text>();
+			//score = score + 100;
+			//score_text.text = "" + score;
 			Destroy(gameObject);
 		}
 	}
 
 	// find out if it gets hit
 	void OnCollisionEnter(Collision c) {
-		Debug.Log (c.collider.name);
-		Debug.Log (c.collider.tag);
+		print (c.collider.name);
+		print (c.collider.tag);
 
 		if (c.collider.tag == "Rocket") {
 			Debug.Log ("ROCKET HIT");
-
+			score = score + 10;
+			score_text.text = "" + score;
 			health = health - 1000.0f;
+		}
+		else if (c.collider.tag == "Rifle") {
+			Debug.Log ("ROCKET HIT");
+			score = score + 4;
+			score_text.text = "" + score;
+			health = health - 400.0f;
+		}
+		else if (c.collider.tag == "Uzi") {
+			Debug.Log ("ROCKET HIT");
+			score = score + 2;
+			score_text.text = "" + score;
+			health = health - 200.0f;
 		}
 	}
 }
---
+++ assets/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float health;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision c) {
+		print ("Player: collision enter");
+		print (c.collider.name);
+		print (c.collider.tag);
+
+		if (c.collider.tag == "Enemy") {
+			Debug.Log ("enemy hit");
+			health = health - 1000.0f;
+		}
+	}
+
+	//Activate the Main function when player is near the door
+	void OnTriggerEnter (Collider c){
+		print ("Player: trigger enter");
+		print (c.name);
+		print (c.tag);
+		if (c.gameObject.tag == "Enemy") {
+			print ("COLLIDED WITH ENEMY LOOL");
+		}
+	}
+
+	//Deactivate the Main function when player is go away from door
+	void OnTriggerExit (Collider c){
+		print ("Player: trigger exit");
+		print (c.name);
+		print (c.tag);
+		if (c.gameObject.tag == "Player") {
+		}
+	}
+}
