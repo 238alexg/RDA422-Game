494f5f14e521a91edb9c591cba6cad61f19d52ce
rghusbands <rghusbands@comcast.net>
2016-05-26 13:38:31 -0700

    UI commit

--- assets/Enemy.cs
+++ assets/Enemy.cs
@@ -1,18 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
 	public float health;
+    private Text score_text;
+    public GameObject ActualScore;
+    int score;
 
 	void Start () {
-		health = 2000.0f;
-
+        ActualScore = GameObject.FindWithTag("Player");
+        score_text = ActualScore.GetComponent <Text>();
+        score = 0;
+        score_text.text = "" + score;
+        health = 3000.0f;
 	}
 
 	void Update () {
 		if (health <= 0.0) {
 			Destroy(gameObject);
+            score = score + 90;
+            score_text.text = "" + score;
 		}
 	}
 
@@ -23,7 +32,8 @@ public class Enemy : MonoBehaviour {
 
 		if (c.collider.tag == "Rocket") {
 			Debug.Log ("ROCKET HIT");
-
+            score = score + 10;
+            score_text.text = "" + score;
 			health = health - 1000.0f;
 		}
 	}
---
+++ assets/PlayerHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int health;
+
+    bool wasHit;
+
+	// Use this for initialization
+	void Start () {
+        health = 100;
+        wasHit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0.0)
+        {
+            Destroy(gameObject);
+            Time.timeScale = 0;
+        }
+    }
+
+    // find out if it gets hit
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log(c.collider.name);
+        Debug.Log(c.collider.tag);
+
+        if (c.collider.tag == "Enemy")
+        {
+            Debug.Log("PLAYER HIT");
+            health = health - 10;
+        }
+    }
+}
